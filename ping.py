85193681172fe672895e00f39938e16842be6760
403-html <tymoteusz.stepien@wp.pl>
2021-02-10 19:31:21 +0100

    Logic (not working yet) and fixing def in class

--- ping.py
+++ ping.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # written in Py 2.7
 import os
+import platform
 import errno
 import time
 from datetime import datetime
@@ -21,25 +22,27 @@ def safe_open_w(path):
     mkdir_p(os.path.dirname(path))
     return open(path, 'w')
 
-with safe_open_w(file_path) as f: pass
-
 def now_date():
     now = datetime.now()
     dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
     return dt_string
+
+safe_open_w(file_path)
 
 class Date_Chunk:
     def __init__ (self, start = None, end = None):
         self.start_date = start
         self.end_date = end
     
-    def update_start_date():
+    def update_start_date(self):
         self.start_date = now_date()
 
-    def update_end_date():
-        self.start_date = now_date()
+    def update_end_date(self):
+        self.end_date = now_date()
 
 ans = None
+loop_count = 0
+
 while True:
     if ans == "q":
         break
@@ -48,10 +51,21 @@ while True:
 
     try:
         response = os.system("ping -c 1 {} > /dev/null".format(hostname))
-        if response == 0:
-            print hostname, 'is up!'
-        else:
-            print hostname, 'is down!'
+
+        loop_count += 1
+        print("Working loop: {}".format(loop_count))
+
+        if not response == 0:
+            print("{}, is down!".format(hostname))
+            if chunk.start_date == None:
+                chunk.update_start_date()
+            elif chunk.end_date == None:
+                chunk.update_end_date()
+            else:
+                with open(file_path, "w") as f:
+                    f.write("Net not working, from {} to {}".format(chunk.start_date, chunk.end_date))
+                    f.close()
+                break
         time.sleep(0.5)
     except KeyboardInterrupt:
         ans = "q"
