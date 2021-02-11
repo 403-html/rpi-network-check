b638fc933107c000dcaaa9e9761ec49e6afd8a82
michal-wybraniec <michal.wybraniec@htdevelopers.com>
2021-02-11 12:37:55 +0100

    Comment deleted, namespaces changed, FILE_PATH can be ommited, instead use function, created path checker

--- ping.py
+++ ping.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
-import os
-import platform
 import errno
+import os
 import time
+import typing
 from datetime import datetime
+
+from custom_logger import logger
 
 # To store config consider python_dotenv
 HOSTNAME = '8.8.8.8'
@@ -27,32 +29,40 @@ def is_path_present(path: str) -> bool:
 def mkdir_p(path):
     try:
         os.makedirs(path)
-    except OSError as exc: # Python >2.5
+    except OSError as exc:  # Python >2.5
         if exc.errno == errno.EEXIST and os.path.isdir(path):
             pass
-        else: raise
+        else:
+            raise exc
+
 
 def safe_open_w(path):
     mkdir_p(os.path.dirname(path))
     return open(path, 'w')
 
-def now_date():
+
+def now_date() -> str:
     now = datetime.now()
     dt_string = now.strftime('%d/%m/%Y %H:%M:%S')
     return dt_string
 
-safe_open_w(file_path)
 
-class Date_Chunk:
-    def __init__ (self, start = None, end = None):
-        self.start_date = start
-        self.end_date = end
-    
-    def update_start_date(self):
+safe_open_w(file_path(FILE_DIR, FILE_NAME))
+
+
+class DateChunk:
+    """TODO: Add description"""
+    start_date: typing.Optional[str]
+    end_date: typing.Optional[str]
+
+    def update_start_date(self) -> None:
+        """TODO: Add description"""
         self.start_date = now_date()
 
-    def update_end_date(self):
+    def update_end_date(self) -> None:
+        """TODO: Add description"""
         self.end_date = now_date()
+
 
 ans = None
 loop_count = 0
@@ -61,24 +71,25 @@ while True:
     if ans == 'q':
         break
 
-    chunk = Date_Chunk()
+    chunk = DateChunk()
 
     try:
-        response = os.system('ping -c 1 {} > /dev/null'.format(hostname))
+        response = os.system(f'ping -c 1 {HOSTNAME} > /dev/null')
 
         loop_count += 1
-        print('Working loop: {}'.format(loop_count))
+        logger.info('Working loop: %s', loop_count)
 
         if not response == 0:
-            print('{}, is down!'.format(hostname))
-            if chunk.start_date == None:
+            logger.info('%s, is down!', HOSTNAME)
+            if chunk.start_date is None:
                 chunk.update_start_date()
-            elif chunk.end_date == None:
+            elif chunk.end_date is None:
                 chunk.update_end_date()
             else:
-                with open(file_path, 'w') as f:
+                # Experimental
+                logger.error('Net not working, from %s to %s', chunk.start_date, chunk.end_date)
+                with open(file_path(FILE_DIR, FILE_NAME), 'w') as f:
                     f.write('Net not working, from {} to {}'.format(chunk.start_date, chunk.end_date))
-                    f.close()
                 break
         time.sleep(0.5)
     except KeyboardInterrupt:
